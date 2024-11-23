543fcb15b4655f0217d56fb5ae2f080f19e6a341
Hartato Adi <hartatoadi@gmail.com>
2024-11-23 21:56:04 +0700

    First Commit, setup Gemfile and Model Definition

---
+++ db/migrate/20241123145100_create_follows.rb
@@ -0,0 +1,12 @@
+class CreateFollows < ActiveRecord::Migration[7.2]
+  def change
+    create_table :follows do |t|
+      t.integer :follower_id, null: false
+      t.integer :followed_id, null: false
+
+      t.timestamps
+    end
+
+    add_index :follows, %i[follower_id followed_id], unique: true
+  end
+end
---
+++ spec/factories/sleep_records.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :sleep_record do
+    user { nil }
+    clock_in { "2024-11-23 21:50:59" }
+    clock_out { "2024-11-23 21:50:59" }
+  end
+end
