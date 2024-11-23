99b994658b337b1f35f975e986610c1926185453
Hartato Adi <hartatoadi@gmail.com>
2024-11-23 22:24:43 +0700

    Fix relation and add namespace versioning routes

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,5 +1,13 @@
 class SleepRecord < ApplicationRecord
   belongs_to :user
+
   validates :clock_in, presence: true
-  scope :last_week, -> { where('clock_in >= ?', 1.week.ago) }
+  validates :clock_out, presence: true
+  validate :clock_out_after_clock_in
+
+  private
+
+  def clock_out_after_clock_in
+    errors.add(:clock_out, "must be after clock_in") if clock_out <= clock_in
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :sleep_records, dependent: :destroy
-  has_many :follows, foreign_key: :follower_id, dependent: :destroy
-  has_many :followed_users, through: :follows, source: :followed
-  has_many :followers, through: :follows, source: :follower
+  has_many :follows, class_name: 'Follow', foreign_key: :follower_id, dependent: :destroy
+  has_many :followees, through: :follows, source: :followee
+  has_many :reverse_follows, class_name: 'Follow', foreign_key: :followed_id, dependent: :destroy
+  has_many :followers, through: :reverse_follows, source: :follower
+
+  validates :name, presence: true, uniqueness: true
 end
